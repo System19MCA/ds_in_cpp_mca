dc8cffe003a2dcfd4b738ac537e6e6183ffd39ae
system19mca <system19mca@gmail.com>
2025-04-28 12:37:52 +0530

    add search algorithms in one file

--- w1_p5.cpp
+++ w1_p5.cpp
@@ -3,33 +3,45 @@
 using namespace std;
 
 template <class T>
-
-int binarySearch(T arr[], int size, T target)
+class Search
 {
-    int start = 0, end = size - 1;
-
-    while (start <= end)
+public:
+    int binarySearch(T arr[], int size, T target)
     {
-        int mid = (start + end) / 2;
-        if (arr[mid] == target)
-            return mid;
-        else if (arr[mid] > target)
-        {
-            end = mid - 1;
-        }
-        else
-        {
-            start = mid + 1;
-        }
-    }
-    return -1;
-}
+        int start = 0, end = size - 1;
 
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    int linearSearch(T arr[], int size, T target)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] == target)
+                return i;
+        }
+        return -1;
+    }
+};
 int main()
 {
 
     int arr[5] = {4, 5, 7, 8, 9};
-    cout << binarySearch<int>(arr, 5, 0);
+    Search<int> s;
+    cout << s.binarySearch(arr, 5, 0);
 
     return 0;
 }
