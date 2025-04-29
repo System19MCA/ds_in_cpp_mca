f7deeacbb6d1ca1fff05b9ca0bd9788aae03ff60
system19mca <system19mca@gmail.com>
2025-04-29 12:23:18 +0530

    add class to string methods

--- w1_p4.cpp
+++ w1_p4.cpp
@@ -2,77 +2,83 @@
 
 using namespace std;
 
-int strlen(char *str)
+class String
 {
-    int len = 0;
-    while (*str++)
-        len++;
-    return len;
-}
 
-int strcmp(char *str1, char *str2)
-{
-    while (*str1 && *str2)
+public:
+    int strlen(char *str)
     {
-        if (*str1 > *str2)
+        int len = 0;
+        while (*str++)
+            len++;
+        return len;
+    }
+
+    int strcmp(char *str1, char *str2)
+    {
+        while (*str1 && *str2)
+        {
+            if (*str1 > *str2)
+                return 1;
+            else if (*str1 < *str2)
+                return -1;
+            *str1++;
+            *str2++;
+        }
+        if (!(*str1) && !(*str2))
+            return 0;
+        if (*str1)
             return 1;
-        else if (*str1 < *str2)
-            return -1;
-        *str1++;
-        *str2++;
+        return -1;
     }
-    if (!(*str1) && !(*str2))
-        return 0;
-    if (*str1)
-        return 1;
-    return -1;
-}
 
-char *strcpy(char *dest, char *src)
-{
-    char *result = dest;
-    while (*src)
+    char *strcpy(char *dest, char *src)
     {
-        *dest = *src;
-        *dest++;
-        *src++;
+        char *result = dest;
+        while (*src)
+        {
+            *dest = *src;
+            *dest++;
+            *src++;
+        }
+        *dest = '\0';
+        return result;
     }
-    *dest = '\0';
-    return result;
-}
 
-char *strcat(char *str1, char *str2)
-{
-    char *result = str1;
-    while (*str1)
-        *str1++;
-    while (*str2)
+    char *strcat(char *str1, char *str2)
     {
-        *str1++ = *str2++;
+        char *result = str1;
+        while (*str1)
+            *str1++;
+        while (*str2)
+        {
+            *str1++ = *str2++;
+        }
+        *str1 = '\0';
+        return result;
     }
-    *str1 = '\0';
-    return result;
-}
 
-char *strrev(char *str)
-{
-
-    int len = strlen(str);
-    for (int i = 0; i <= len / 2; i++)
+    char *strrev(char *str)
     {
-        swap(str[i], str[len - 1 - i]);
+
+        int len = strlen(str);
+        for (int i = 0; i <= len / 2; i++)
+        {
+            swap(str[i], str[len - 1 - i]);
+        }
+        return str;
     }
-    return str;
-}
+};
 
 int main()
 {
     char s[] = "hello";
     char p[] = "psdf";
-    cout << strlen(s) << endl;
-    cout << strcmp(s, p) << endl;
+    String ob;
+    cout << ob.strlen(s) << endl;
+    cout << ob.strcmp(s, p) << endl;
     // cout << strcpy(s, p);
-    cout << strcat(s, p) << endl;
-    cout << strrev(s) << endl;
+    cout << ob.strcat(s, p) << endl;
+    cout << ob.strrev(s) << endl;
     return 0;
 }
